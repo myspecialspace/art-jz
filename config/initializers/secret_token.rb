4735f7497d82015cf1ec35592c81b98c989831b3
myspecialspace <ek.a.kuznetsova@gmail.com>
2014-08-26 16:35:55 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ArtJz::Application.config.secret_key_base = 'd99ecea9a4ce46424f8e3e261fc45a5c25f9d36a9fb36ebca28e6c6840fda563fc64e299f27ec772d0f287501e4c0436da27a4ed1f7396b6dcae2dd16886148e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ArtJz::Application.config.session_store :cookie_store, key: '_art-jz_session'
